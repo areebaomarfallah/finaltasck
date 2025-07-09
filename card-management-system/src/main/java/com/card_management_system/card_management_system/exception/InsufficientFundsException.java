6822d3d59d681213be4bc0054da7b7d716236fec
areebaomarfallah <ofallah@areeba.com>
2025-07-09 14:24:15 +0300

    update old card_management_system

---
+++ card-management-system/src/main/java/com/card_management_system/card_management_system/exception/InsufficientFundsException.java
@@ -0,0 +1,11 @@
+package com.card_management_system.card_management_system.exception;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class InsufficientFundsException extends RuntimeException {
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+
+}
---
+++ card-management-system/src/main/java/com/card_management_system/card_management_system/exception/InvalidTransactionException.java
@@ -0,0 +1,7 @@
+package com.card_management_system.card_management_system.exception;
+
+public class InvalidTransactionException extends RuntimeException {
+    public InvalidTransactionException(String message) {
+        super(message);
+    }
+}
